a796d317e9a2fdad9f34d457b7e254a1ac2ff428
almarauder <darking.picqaxe@gmail.com>
2026-06-14 21:56:42 +0500

    Adding API Constants and Details

--- lib/data/api/api_constant.dart
+++ lib/data/api/api_constant.dart
@@ -1,4 +1,19 @@
-class ApiConstants {
-  static const String baseUrl = 'http://172.20.10.12:8080';
-  static const String authEndpoint = '/api/auth';
+class ApiConstant {
+  static const String baseUrl = 'https://happy-forgiveness-production-caca.up.railway.app/api';
+
+  // Auth
+  static const String register = '$baseUrl/auth/register/';
+  static const String login = '$baseUrl/auth/login/';
+  static const String me = '$baseUrl/auth/me/';
+  static const String registerContractor = '$baseUrl/auth/register-contractor/';
+
+  // Contracts
+  static const String contractTemplates = '$baseUrl/contracts/templates/';
+  static const String contracts = '$baseUrl/contracts/';
+  static String contractDetail(int id) => '$baseUrl/contracts/$id';
+  static String contractSign(int id) => '$baseUrl/contracts/$id/sign/';
+
+  // Tasks
+  static const String tasks = '$baseUrl/tasks/';
+  static String taskDetail(int id) => '$baseUrl/tasks/$id';
 }
